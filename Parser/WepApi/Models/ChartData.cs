8dcb55a5ab959a506e352b666325b90eacc78400
Nazgrim <serega19911991@ya.ru>
2016-10-08 15:39:06 +0300

    asd

--- Parser/WepApi/Models/ChartData.cs
+++ Parser/WepApi/Models/ChartData.cs
@@ -7,8 +7,8 @@ namespace WepApi.Models
         public IEnumerable<int> seriesData { get; set; }
         public double min { get; set; }
         public double max { get; set; }
-        public int msrpPrice { get; set; }
+        public double msrpPrice { get; set; }
         public double avrPrice { get; set; }
-        public int dealerPrice { get; set; }
+        public double dealerPrice { get; set; }
     }
 }
